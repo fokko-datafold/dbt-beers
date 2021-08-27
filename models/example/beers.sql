682bc9052e9dec4fc19cb90a4be4fafc816fd3a0
Fokko Driesprong <fokko@datafold.com>
2021-08-27 19:33:59 +0200

    Use compound tests

--- models/example/beers.sql
+++ models/example/beers.sql
@@ -4,13 +4,12 @@
 ) }}
 
 SELECT
-  id            AS "beer id",
+  id            AS beer_id,
   name          AS beer_name,
   style         AS beer_style,
   abv           AS abv,
   ibu           AS ibu,
   brewery_id    AS brewery_id,
-  ounces        AS ounces,
-  '1'           AS "number one"
+  ounces        AS ounces
 FROM
   {{ ref('seed_beers') }}
